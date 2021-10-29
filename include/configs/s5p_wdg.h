30ccdbb2a7b7028cea0aa8ccf97cdee199d04394
winddoing <winddoing@sina.cn>
2021-10-29 10:56:17 +0800

    enable debug info

--- include/configs/s5p_wdg.h
+++ include/configs/s5p_wdg.h
@@ -15,6 +15,7 @@
 #define CONFIG_S5P		1	/* which is in a S5P Family */
 #define CONFIG_S5PC110		1	/* which is in a S5PC110 */
 
+#define DEBUG
 
 #include <linux/sizes.h>
 #include <asm/arch/cpu.h>		/* get chip and board defs */
